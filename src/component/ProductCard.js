86931b35fc186959fccb0c1339d8fad14aaa296f
홍기문(Kimoon Hong) <153352715+KimoonH@users.noreply.github.com>
2024-03-27 20:47:56 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img src="https://tooneul.com/web/product/big/202312/fe809b54954800922abcab9c8fb5e1e8.jpg" />
+            <div>Consicious choice</div>
+            <div>벨티드 트월 코트</div>
+            <div>₩99900</div>
+            <div>신제품</div>
+        </div>
+    )
+}
+
+export default ProductCard
